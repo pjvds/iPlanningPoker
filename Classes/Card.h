0cb6c5ca0c717973ee6ab3087a83fedb5296db7c
pjvds <pj@born2code.net>
2010-11-16 21:52:52 +0100

    Added Card.m. It will be the new place for the card logic. Next step, change MainScene to use this class.

---
+++ Classes/Card.h
@@ -0,0 +1,18 @@
+//
+//  Card.h
+//  iPlanningPoker
+//
+//  Created by Pieter Joost van de Sande on 11/14/10.
+//  Copyright Born2code.net 2010. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// MainScene Layer
+@interface Card : CCNode
+{
+}
+
+@end
---
+++ Classes/Card.m
@@ -0,0 +1,32 @@
+//
+//  Card.m
+//  iPlanningPoker
+//
+//  Created by Pieter Joost van de Sande on 11/15/10.
+//  Copyright 2010 Born2code.net. All rights reserved.
+//
+
+#import "Card.h"
+
+CCSprite *background;
+
+@implementation Card
+
++(id) node
+{
+	return [[[self alloc] init] autorelease];
+}
+
+-(Card*) init {
+	// always call "super" init
+	// Apple recommends to re-assign "self" with the "super" return value
+	if( (self=[super init] )) {
+        background = [CCSprite spriteWithFile:@"CardBackground.png"];
+		background.position = ccp(background.contentSize.width / 2,background.contentSize.height / 2);
+		[self addChild: background];
+    }
+    
+    return self;
+}
+
+@end
